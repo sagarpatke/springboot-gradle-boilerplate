da6c571162b5bf79c98dcfc0687f895a8521f50b
Sagar Patke <sagarpatke@gmail.com>
2017-10-15 14:44:53 +0530

    SpringBoot Starter Web with Gradle, Java9 and JUnit 5

--- src/main/java/in/stackroute/learning/Hello.java
+++ src/main/java/in/stackroute/learning/Hello.java
@@ -1,12 +1,25 @@
 package in.stackroute.learning;
 
-class Hello {
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Hello {
 
   public String helloMessage() {
     return "Hello, Gradle!";
   }
 
   public static void main(String... args) {
+    SpringApplication.run(Hello.class, args);
     System.out.println("Hello, Gradle!");
+  }
+
+  @RequestMapping("/")
+  public static String sayHello() {
+    return "Hello, SpringBoot!";
   }
 }
